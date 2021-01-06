2b47a15f3046c3ffe7d304d81197c92e8937750c
Surarchita2 <69779870+Surarchita2@users.noreply.github.com>
2021-01-05 20:51:14 -0800

    Update Tree.js

--- Tree.js
+++ Tree.js
@@ -7,7 +7,7 @@ class Tree{
         this.body = Bodies.rectangle(x,y,200,600,options);
         this.width=200
         this.height=600
-        this.image = loadImage("Plucking_mangoes/tree.png")
+        this.image = loadImage("tree.png")
         World.add(world,this.body)
     }
 display(){
